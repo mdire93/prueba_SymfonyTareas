f95282aa92b346dc3120ba980a2151a768d9ea49
mdire93 <m.dire9302@gmail.com>
2021-10-21 23:01:37 +0200

    Update and rename bbdd_kosaas.sql to bbdd_tareas.sql

--- bbdd_tareas.sql
+++ bbdd_tareas.sql
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bbdd_kosaas`
+-- Base de datos: `bbdd_tareas`
 --
 CREATE DATABASE IF NOT EXISTS `bbdd_tareas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `bbdd_kosaas`;
+USE `bbdd_tareas`;
 
 -- --------------------------------------------------------
 
